56e6e04b37e2c2d9efb8ccc405151c6be7979598
CryztalGalaxy <crystals33861@gmail.com>
2022-10-16 16:21:19 -0400

    three functions
    functions.test(two params of same type)
    functions.a(three params of string or list of strings)
    functions.b(one param of any type)
    functions.c(three params of string or list of strings)

--- functions/functions/functions.py
+++ functions/functions/functions.py
@@ -1,3 +1,22 @@
 
+import random;
+
 def test(a, b):
 	print(a + b);
+
+def a(a, b, c):
+	for i in range(10):
+		v = random.choice(a, b, c)
+		if i == 0:
+			word = v[random.randint(v.length)];
+		else:
+			word += v[random.randint(v.length)];
+		return word;
+def b(a):
+	r = a;
+	for i in range(random.randint(3)):
+		r += a;
+	return r;
+def c(a, b, c):
+	ans = input(a(c, b, a));
+	print(a(random.choice(a, b, c, ans), random.choice(a, b, c, ans), random.choice(a, b, c, ans)));
